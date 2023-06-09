442b75883fa24a4e8e47396d64838c6000e672b6
Aleksandar <a.garkov@student.fontys.nl>
2023-06-09 15:26:28 +0200

    final final xd

--- RazorPagesProject/RazorPagesProject/Pages/LogIn.cshtml.cs
+++ RazorPagesProject/RazorPagesProject/Pages/LogIn.cshtml.cs
@@ -8,6 +8,11 @@ using System.Security.Claims;
 using RazorPagesProject.Models;
 using ClassLibrary.Controllers;
 using RazorPagesProject.Helpers;
+using ClassLibrary.Models;
+using ClassLibrary.Mapper;
+using DataBaseClassLibrary.DatabaseHelpers;
+using DataBaseClassLibrary.Interfaces;
+using DataBaseClassLibrary.DTOs;
 
 namespace RazorPagesProject.Pages
 {
@@ -18,9 +23,17 @@ namespace RazorPagesProject.Pages
 
         [BindProperty]
         public CredentialsViewModel Credentials { get; set; }
-        
+
+        internal UserDatabaseHelper userDbHelper;
+        internal GradeDatabaseHelper gradeDbHelper;
+
+        internal UserMapper userMapper;
         public LogInModel()
         {
+            this.userDbHelper = new UserDatabaseHelper();
+            this.gradeDbHelper = new GradeDatabaseHelper();
+
+            this.userMapper = new UserMapper(userDbHelper, gradeDbHelper);
         }
 
         public IActionResult OnGet()
@@ -45,33 +58,40 @@ namespace RazorPagesProject.Pages
         {
             if (ModelState.IsValid)
             {
-                #region handle rememberme cookie
-                if (this.Credentials.RememberMe)
+                foreach (ManagerDTO managerDTO in userDbHelper.GetAllManagersFromDB())
                 {
-                    CookieOptions cookieOptions = new CookieOptions();
-                    cookieOptions.Expires = DateTime.Now.AddDays(4);
-                    Response.Cookies.Append(RememberMeCookiename, this.Credentials.Email, cookieOptions);
-                }
-                else
-                {
-                    if (Request.Cookies.ContainsKey(RememberMeCookiename))
+                    if (managerDTO.Email == Credentials.Email && managerDTO.Password == Credentials.Password)
                     {
-                        Response.Cookies.Delete(RememberMeCookiename);
-                    }
-                }
-                #endregion
+                        #region handle rememberme cookie
+                        if (this.Credentials.RememberMe)
+                        {
+                            CookieOptions cookieOptions = new CookieOptions();
+                            cookieOptions.Expires = DateTime.Now.AddDays(4);
+                            Response.Cookies.Append(RememberMeCookiename, this.Credentials.Email, cookieOptions);
+                        }
+                        else
+                        {
+                            if (Request.Cookies.ContainsKey(RememberMeCookiename))
+                            {
+                                Response.Cookies.Delete(RememberMeCookiename);
+                            }
+                        }
+                        #endregion
 
-                List<Claim> claims = new IdentityManager().Authenticate(this.Credentials.MapToDomain());
-                if (claims.Count > 0)
-                {
-                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                    HttpContext.SignInAsync(new ClaimsPrincipal(claimsIdentity));
-                    return new RedirectToPageResult("Index");
-                }
-                else
-                {
-                    ViewData["ErrorMessage"] = "Unable to login.";
-                    return Page();
+                        List<Claim> claims = new IdentityManager().Authenticate(this.Credentials.MapToDomain());
+                        if (claims.Count > 0)
+                        {
+                            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                            HttpContext.SignInAsync(new ClaimsPrincipal(claimsIdentity));
+                            return new RedirectToPageResult("Index");
+                        }
+                        else
+                        {
+                            ViewData["ErrorMessage"] = "Unable to login.";
+                            return Page();
+                        }
+                    }
+                    
                 }
             }
             return Page();
