7ae242f94fb19c56d7ed812ebe9c5451d351c6eb
Aleksandar <a.garkov@student.fontys.nl>
2023-06-09 14:54:31 +0200

    final version

--- RazorPagesProject/DataBaseClassLibrary/DatabaseHelpers/SqlService.cs
+++ RazorPagesProject/DataBaseClassLibrary/DatabaseHelpers/SqlService.cs
@@ -9,7 +9,7 @@ namespace DataBaseClassLibrary.DatabaseHelpers
 {
     public class SqlService
     {
-        private readonly string connectionString = "Server=localhost;Database=individual_project_semester2;Trusted_Connection=True;";
+        private readonly string connectionString = "Server=mssqlstud.fhict.local;Database=dbi501079_individual;User Id=dbi501079_individual;Password=password;";
         public SqlConnection connectionFactory()
         {
             return new SqlConnection(connectionString);
